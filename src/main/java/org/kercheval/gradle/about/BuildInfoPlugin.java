1d4aa4a61e55a2be172408f48c83478d874a26f5
John Kercheval <kercheval@gmail.com>
2012-10-16 16:35:40 -0700

    Intermediate state - Added test build.gradle

--- src/main/java/org/kercheval/gradle/about/BuildInfoPlugin.java
+++ src/main/java/org/kercheval/gradle/about/BuildInfoPlugin.java
@@ -1,12 +1,13 @@
 package org.kercheval.gradle.about;
 
 import org.gradle.api.Plugin;
+import org.gradle.api.Project;
 
-public class BuildInfoPlugin implements Plugin {
+public class BuildInfoPlugin implements Plugin<Project> {
     @Override
-    public void apply(Object target) {
+    public void apply(Project project) {
+        System.out.println("%%%%%%%%%%%%%  Apply Called for " + project.getName());
 
-//      Project project = (Project) target;
 //      project.t
 //      target.task("buildInfo", )
         // TODO Auto-generated method stub
