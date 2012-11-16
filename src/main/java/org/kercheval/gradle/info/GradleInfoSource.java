036d936676eb1b9f0dc3e8ca11c897eab45aa8b1
John Kercheval <kercheval@gmail.com>
2012-11-16 13:56:22 -0800

    Remove unnecessary println

--- src/main/java/org/kercheval/gradle/info/GradleInfoSource.java
+++ src/main/java/org/kercheval/gradle/info/GradleInfoSource.java
@@ -85,7 +85,6 @@ public class GradleInfoSource
 
 		for (final Task task : project.getAllTasks(false).get(project))
 		{
-			System.out.println(task.getName());
 			tasknameMap.put(task.getName(), task);
 		}
 
